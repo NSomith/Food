a11a7ccf65ef7ce60f6e14bd4dc6e1266d703fb0
N Somith Singh <ningombamsomith@gmail.com>
2022-08-07 11:57:25 +0530

    45:57

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,41 @@
 import React from 'react'
 import logo from './img/logo.png'
+import { MdShoppingBasket } from "react-icons/md";
 
 const Header = () => {
     return (
-        <div className='fixed w-screen z-50 p-6 px-16'>
+        <header className='fixed w-screen z-50 p-6 px-16'>
             {/* desktop and tab  */}
-            <div className='hidden md:flex w-full  h-full'>
+            <div className=' hidden md:flex w-full  h-full items-center justify-between'>
                 <div className='flex items-center gap-2'>
-                    <img src={logo} alt="logo" className='w-10 object-contain' />
+                    <img src={logo} alt="logo" className='w-10 object-cover' />
                     <p className='text-headingColor text-xl font-bold'>City</p>
+                </div>
+
+                <div className='flex items-center'>
+                    <ul className='flex items-center  gap-8'>
+                        <li className='text-base text-textColor hover:text-headingColor duration-100
+                    transition-all ease-in-out cursor-pointer'>Home</li>
+                        <li className='text-base text-textColor hover:text-headingColor duration-100
+                    transition-all ease-in-out cursor-pointer'>Menu</li>
+                        <li className='text-base text-textColor hover:text-headingColor duration-100
+                    transition-all ease-in-out cursor-pointer'>About Us</li>
+                        <li className='text-base text-textColor hover:text-headingColor duration-100
+                    transition-all ease-in-out cursor-pointer'>Service</li>
+                    </ul>
+
+                    <div className='relative flex items-center justify-center'>
+                        <MdShoppingBasket className='text-textColor text-2xl ml-8 cursor-pointer' />
+                        <div className='absolute -top-2 left-10 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center'>
+                            <p className='text-xs text-white font-semibold'>2</p>
+                        </div>
+                    </div>
                 </div>
             </div>
 
             {/* for mobile */}
-            <div className='flex md:hidden w-full  h-full'></div>
-        </div>
+            <div className='flex md:hidden w-full  h-full  '></div>
+        </header>
     )
 }
 
